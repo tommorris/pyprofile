cdc6fdb0a0e14d44026965bd75dd566c7a4a11b3
Tom Morris <tom@tommorris.org>
2008-03-27 18:35:25 +0000

    created a detectRss() method, which looks through RSS feeds to try and figure out people's names. Added new tests

--- lib/ProfileGrab.py
+++ lib/ProfileGrab.py
@@ -15,12 +15,15 @@ Name parsing ideas
 """
 
 from BeautifulSoup import BeautifulSoup
-import sys, os, unittest, urllib2, re
+from DiskCacheFetcher import DiskCacheFetcher
+from operator import itemgetter
+import sys, os, unittest, urllib2, re, feedparser, autorss
 
 class ProfileGrab:
   def __init__(self, uri):
+    self.fetcher = DiskCacheFetcher('/tmp')
     self.uri = uri
-    self.data = urllib2.urlopen(self.uri)
+    self.data = self.fetcher.fetch(uri, 43200)
     # presume it's html for now, we'll refactor to take into account other stuff
     self.soup = BeautifulSoup(self.data)
     self.author = None
@@ -33,6 +36,10 @@ class ProfileGrab:
     # hCard
     if self.author is None:
       self.hcard()
+    
+    # 4. Look for RSS feeds and parse the names out of there
+    if self.author is None:
+      self.detectRss()
   
   def hcard(self):
     # declare
@@ -97,6 +104,32 @@ class ProfileGrab:
     regex_hcard = re.compile('vcard')
     self.hcards = self.soup.findAll(True, {'class': regex_hcard})
   
+  def detectRss(self):
+    self.rss_feed = autorss.getRSSLinkFromHTMLSource(self.data)
+    firstfeed = feedparser.parse(self.rss_feed, 43200)
+    if hasattr(firstfeed, 'author_detail') is True:
+      if firstfeed.author_detail.name is not None:
+        self.author = unicode(firstfeed.author_detail.name)
+    else:
+      authorarray = []
+      for i in firstfeed.entries:
+        if hasattr(i, 'author'):
+          authorarray += [i.author]
+      if len(authorarray) is not 0:
+        self.author = unicode(getMostPopularFromList(authorarray))
+  
+
+
+def getMostPopularFromList(inlist):
+  output = {}
+  for i in inlist:
+    if i in output:
+      output[i] = (output.get(i) + 1)
+    else:
+      output[i] = 1
+  items = output.items()
+  items.sort(key = itemgetter(1), reverse=True)
+  return items[0][0]
 
 
 class ProfileGrabTests(unittest.TestCase):
@@ -110,13 +143,20 @@ class ProfileGrabTests(unittest.TestCase):
     self.assertEqual(ProfileGrab("http://suda.co.uk/").author, u"Brian Suda")
   def test_tantek(self):
     self.assertEqual(ProfileGrab("http://tantek.com/").author, u"Tantek Çelik")
+  def test_scoble(self):
+    self.assertEqual(ProfileGrab("http://scobleizer.com/").author, u"Robert Scoble")
   def test_me(self):
     self.assertEqual(ProfileGrab("http://tommorris.org/").author, u"Tom Morris")
-#	  self.assertEqual(ProfileGrab("http://factoryjoe.com/"), u"Chris Messina")
-#	  self.assertEqual(ProfileGrab("http://www.cubicgarden.com/"), u"Ian Forrester")
-#	  self.assertEqual(ProfileGrab("http://www.andybudd.com/"), u"Andy Budd")
-#	  self.assertEqual(ProfileGrab("http://scienceblogs.com/pharyngula/"), u"PZ Myers")
-#   self.assertEqual(ProfileGrab("http://aralbalkan.com/"), u"Aral Balkan")
+  def test_dave(self):
+    self.assertEqual(ProfileGrab("http://scripting.com/").author, u"Dave Winer")
+  def test_chrismessina(self):
+    self.assertEqual(ProfileGrab("http://factoryjoe.com/blog/").author, u"Chris Messina")
+  def test_ianforrester(self):
+    self.assertEqual(ProfileGrab("http://www.cubicgarden.com/").author, u"Ian Forrester")
+  def test_andybudd(self):
+    self.assertEqual(ProfileGrab("http://www.andybudd.com/").author, u"Andy Budd")	
+#	  self.assertEqual(ProfileGrab("http://scienceblogs.com/pharyngula/").author, u"PZ Myers")
+#   self.assertEqual(ProfileGrab("http://aralbalkan.com/").author, u"Aral Balkan")
 
 
 if __name__ == '__main__':
